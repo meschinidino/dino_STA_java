96a79141d1afcbf2925cfdd119fa24abaf4645cb
meschinidino <francodmsoloing@gmail.com>
2024-09-29 21:21:03 -0300

    agregando alumnos + profesores para cumplir tarea

--- src/main/java/com/example/um1/um1Application.java
+++ src/main/java/com/example/um1/um1Application.java
@@ -1,6 +1,9 @@
 package com.example.um1;
 
+import com.example.um1.entidades.Alumno;
 import com.example.um1.entidades.Profesor;
+
+import com.example.um1.enumeraciones.Especialidades;
 import com.example.um1.enumeraciones.Titulos;
 import com.example.um1.repositorios.AlumnoRepository;
 import com.example.um1.repositorios.PersonaRepository;
@@ -57,6 +60,38 @@ public class um1Application {
 			// Lo grabo a través del repositorio de Spring
 			profesorRepository.save(pro1);
 
+			Profesor pro2 = Profesor.builder()
+					.nombre("NotAlberto")
+					.nombre("Cortez")
+					.fechaIngreso(LocalDate.of(2022, 1, 1))
+					.cantHijos(2)
+					.titulo(Titulos.MASTER)
+					.sueldo(new BigDecimal("123.45"))
+					.build();
+
+
+			// Lo grabo a través del repositorio de Spring
+			profesorRepository.save(pro2);
+
+			Alumno al1 = Alumno.builder()
+					.nombre("Dino")
+					.apellido("Meschini")
+					.legajo(62000)
+					.especialidad(Especialidades.BACHILLER)
+					.build();
+
+			// Lo grabo a través del repositorio de Spring
+			alumnoRepository.save(al1);
+
+			Alumno al2 = Alumno.builder()
+					.nombre("Dinon't")
+					.apellido("Meschini")
+					.legajo(62001)
+					.especialidad(Especialidades.BACHILLER)
+					.build();
+
+			// Lo grabo a través del repositorio de Spring
+			alumnoRepository.save(al2);
 
 		};
 
